473722a4634db0896799eb3870bd6f33e06cb7e6
anclaro <anclaro@iu.edu>
2023-02-08 13:25:00 -0500

    deleted py file

--- simple_oscillator_task/visualize.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-# Read data from file
-evolvedAvgFitness = np.load("evolvedAvgFitness.npy")
-evolvedFitness = np.load("evolvedFitness.npy")
-learnedAvgFitness = np.load("learnedAvgFitness.npy")
-learnedFitness = np.load("learnedFitness.npy")
